4cb7949a3cfd7fa01345a3a3756610eee4477eaa
Joe Pringle <joe@joepringle.dev>
2020-06-15 18:37:25 -0400

    link to source code

--- frontend/src/features/header/Header.tsx
+++ frontend/src/features/header/Header.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Navbar, Button, Alignment } from '@blueprintjs/core';
 
+const onClick = () => { window.open('https://github.com/joechip504/learns2', '_blank')}
+
 export const Header = () => {
     return (
         <Navbar className='bp3-dark'>
@@ -11,7 +13,7 @@ export const Header = () => {
             </Navbar.Group>
             <Navbar.Group align={Alignment.RIGHT}>
                 <Button className='bp3-minimal' icon='info-sign' text='About' />
-                <Button className='bp3-minimal' icon='git-repo' text='Source' />
+                <Button className='bp3-minimal' icon='git-repo' text='Source' onClick={onClick}/>
             </Navbar.Group>
         </Navbar>
     )
