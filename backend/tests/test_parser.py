c46a52aba948e5514c2328912f822473ec879efb
Joe Pringle <explodingp@gmail.com>
2020-09-25 19:20:26 -0400

    wip parser - just test if we can read protocol

--- backend/tests/test_parser.py
+++ backend/tests/test_parser.py
@@ -1,5 +1,11 @@
 from learns2.parser import SC2ReplayParser
 
-def test_return_hello():
-    parser = SC2ReplayParser()
-    assert parser.return_hello() == 'hello'
+from pathlib import Path
+
+resources = Path(__file__).absolute().parent.joinpath('resources')
+
+
+def test_proto_81433():
+    replay = resources.joinpath('EverDream.SC2Replay')
+    parser = SC2ReplayParser(replay)
+    assert "protocol81433" in str(parser.protocol)
