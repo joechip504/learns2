8b6e0a6fb4f9f3e397f807b79ff71a35960ad8c8
Joe Pringle <explodingp@gmail.com>
2020-09-26 15:19:14 -0400

    parse players and subset of replay details

--- backend/learns2/parser.py
+++ backend/learns2/parser.py
@@ -2,16 +2,73 @@ from s2protocol import versions
 from mpyq import MPQArchive
 import os
 
+from typing import List
+
 
 class SC2ReplayParser(object):
-    def __init__(self, replay):
+    def __init__(self, replay: str):
         assert os.path.isfile(replay), f'{replay} is not a file'
         self.archive = MPQArchive(replay)
-        self.protocol = self.get_protocol(self.archive)
+        self.protocol = self.read_protocol(self.archive)
+        self._initdata = None
+        self._details = None
 
     @staticmethod
-    def get_protocol(archive: MPQArchive):
+    def read_protocol(archive: MPQArchive):
         content = archive.header['user_data_header']['content']
         header = versions.latest().decode_replay_header(content)
         base_build = header['m_version']['m_baseBuild']
         return versions.build(base_build)
+
+    @staticmethod
+    def read_initdata(protocol, archive: MPQArchive) -> dict:
+        init_file = archive.read_file('replay.initData')
+        return protocol.decode_replay_initdata(init_file)
+
+    @staticmethod
+    def read_details(protocol, archive: MPQArchive) -> dict:
+        detail_file = archive.read_file('replay.details')
+        return protocol.decode_replay_details(detail_file)
+
+    def initdata(self) -> dict:
+        if self._initdata is None:
+            self._initdata = self.read_initdata(self.protocol, self.archive)
+        return self._initdata
+
+    def details(self) -> dict:
+        """https://github.com/Blizzard/s2protocol/blob/master/docs/flags/details.md"""
+        if self._details is None:
+            self._details = self.read_details(self.protocol, self.archive)
+        return self._details
+
+    def players(self) -> List[dict]:
+        slots = self.initdata()['m_syncLobbyState']['m_lobbyState']['m_slots']
+        slot_id_to_user_id = {}
+        for slot in slots:
+            if slot['m_observe'] == 0:
+                slot_id, user_id = slot['m_workingSetSlotId'], slot['m_userId']
+                slot_id_to_user_id[slot_id] = user_id
+        player_list = self.details()['m_playerList']
+        player_objects = []
+        for player in player_list:
+            slot_id = player['m_workingSetSlotId']
+            if slot_id in slot_id_to_user_id:
+                player['m_userId'] = slot_id_to_user_id[slot_id]
+                player['m_userInitialData'] = self.initdata()['m_syncLobbyState']['m_userInitialData'][slot_id]
+                player_objects.append(player)
+        return player_objects
+
+    def small_details(self):
+        d = self.details()
+        return {
+            'm_title': d['m_title'],
+            'm_isBlizzardMap': d['m_isBlizzardMap'],
+            'm_timeUTC': d['m_timeUTC'],
+            'm_timeLocalOffset': d['m_timeLocalOffset']
+        }
+
+    def to_dict(self):
+        return {
+            'details': self.small_details(),
+            'players': self.players()
+        }
--- backend/tests/test_parser.py
+++ backend/tests/test_parser.py
@@ -3,9 +3,27 @@ from learns2.parser import SC2ReplayParser
 from pathlib import Path
 
 resources = Path(__file__).absolute().parent.joinpath('resources')
+everdream = resources.joinpath('EverDream.SC2Replay')
 
 
 def test_proto_81433():
-    replay = resources.joinpath('EverDream.SC2Replay')
-    parser = SC2ReplayParser(replay)
+    parser = SC2ReplayParser(everdream)
     assert "protocol81433" in str(parser.protocol)
+
+
+def test_ladder_1v1_81433():
+    parser = SC2ReplayParser(everdream)
+    payload = parser.to_dict()
+    details = payload['details']
+    players = payload['players']
+    assert len(players) == 2
+    p0, p1 = players[0], players[1]
+    assert details['m_title'].decode('utf-8') == 'Ever Dream LE'
+    assert details['m_isBlizzardMap']
+    assert details['m_timeUTC'] == 132436435995866825
+    assert p0['m_userInitialData']['m_name'].decode('utf-8') == 'GamIsSecHome'
+    assert p1['m_userInitialData']['m_name'].decode('utf-8') == 'Jobama'
+    assert p0['m_userId'] == 0
+    assert p1['m_userId'] == 1
+    assert p0['m_race'].decode('utf-8') == 'Protoss'
+    assert p1['m_race'].decode('utf-8') == 'Protoss'
