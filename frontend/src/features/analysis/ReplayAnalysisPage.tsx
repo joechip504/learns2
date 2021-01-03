3e23acbfc9da587e317d8b341766e4089755faba
Joe Pringle <explodingp@gmail.com>
2021-01-03 17:25:32 -0500

    placeholder analysis table, colors, and description

--- frontend/src/features/analysis/ReplayAnalysisPage.tsx
+++ frontend/src/features/analysis/ReplayAnalysisPage.tsx
@@ -5,7 +5,7 @@ import { useRouteMatch } from 'react-router-dom';
 import { constants } from '../../app/constants';
 import { useDocumentData, useDocumentDataOnce } from 'react-firebase-hooks/firestore';
 import { Prediction, Replay, ReplayPlayer } from '../../interfaces/Replay';
-import { Callout, Card, Divider, Intent, Spinner } from '@blueprintjs/core';
+import { Callout, Card, Divider, Icon, IconName, Intent, Spinner } from '@blueprintjs/core';
 import Player, { AllPlayersCache } from '../../interfaces/Player';
 
 interface RouteParams {
@@ -17,6 +17,19 @@ interface AnalysisProps {
     playerCache: AllPlayersCache;
 }
 
+const AnalysisDescription = (confidence: number, player: Player) => {
+    const link = <a href={player.url} target='_blank' rel="noopener noreferrer">{player.name}</a>
+    if (confidence >= .95) {
+        return <p>This replay exhibits <b>high</b> similarity to known games of {link}.</p>
+    }
+    else if (confidence >= .85) {
+        return <p>This replay exhibits <b>some</b> similarity to known games of {link}.</p>
+    }
+    else {
+        return <p>This replay exhibits <b>a little</b> similarity to known games of {link}.</p>
+    }
+}
+
 const Overview = (replay: Replay) => {
     const details = replay.details;
     // Hell if I know
@@ -26,8 +39,9 @@ const Overview = (replay: Replay) => {
     return (
         <Callout icon='application'>
             <h4 className='bp3-heading'>{details.m_title}</h4>
-            <Divider />
             <p>{date}</p>
+            <Divider />
+
         </Callout>
     )
 };
@@ -44,36 +58,78 @@ const profileUrl = (localId: string) => {
     return `${prefix}/${region}/profile/${localId}`;
 }
 
+const PlayerInfoTable = (props: { player: ReplayPlayer }) => {
+    const result = props.player.m_result === 1 ? "WIN" : "LOSS";
+
+    return (
+        <table className="bp3-html-table bp3-html-table-striped">
+            <thead>
+                <th>Result</th>
+                <th>Race</th>
+                <th>MMR</th>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>{result}</td>
+                    <td>{props.player.m_race}</td>
+                    <td>{props.player.m_userInitialData.m_scaledRating}</td>
+                </tr>
+            </tbody>
+        </table>
+    )
+}
+
+const PlayerPredictionTable = (props: { player: ReplayPlayer, playerLookup: Map<string, Player>, prediction?: Prediction }) => {
+    if (props.prediction === undefined) return null;
+    const player = props.playerLookup.get(props.prediction.player_doc_id)!;
+    const strConfidence = (100 * props.prediction.confidence).toFixed(2) + '%';
+    let rowClass = 'tr-high-confidence';
+    let icon: IconName = 'person';
+    if (props.prediction.confidence < .95) {
+        rowClass = 'tr-med-confidence';
+        icon = 'help';
+        if (props.prediction.confidence < .85) {
+            rowClass = 'tr-low-confidence';
+            icon = 'error';
+        }
+    }
+    return (
+        <table className="bp3-html-table bp3-html-table-striped">
+            <thead>
+                <th></th>
+                <th>Prediction</th>
+                <th>Confidence</th>
+            </thead>
+            <tbody>
+                <tr className={rowClass}>
+                    <td><Icon icon={icon} /></td>
+                    <td>{player.name}</td>
+                    <td>{strConfidence}</td>
+                </tr>
+            </tbody>
+        </table>
+    )
+}
+
 const PlayerCard = (player: ReplayPlayer, idx: number, playerLookup: Map<string, Player>, prediction?: Prediction) => {
     let playerName = player.m_userInitialData.m_name;
     if (player.m_userInitialData.m_clanTag) {
         playerName = `<${player.m_userInitialData.m_clanTag}> ${playerName}`;
     }
     const url = profileUrl(player.m_localizedId);
-    const race = player.m_race;
-    const mmr = player.m_userInitialData.m_scaledRating;
-    const result = player.m_result === 1 ? "WIN" : "LOSS";
-    let stub = race;
-    if (mmr && mmr > 0) {
-        stub = `${mmr} ` + stub;
-    };
-    let analysis = null;
-    if (prediction !== undefined) {
-        const player = playerLookup.get(prediction.player_doc_id);
-        if (player) {
-            const jsonPlayer = JSON.stringify(player);
-            analysis = <p>{`confidence=${prediction.confidence}, player=${jsonPlayer}`}</p>
-        }
-    };
+    const blurb = prediction && AnalysisDescription(prediction.confidence, playerLookup.get(prediction.player_doc_id)!);
     return (
         <Card className='analysis-player-card' key={idx}>
             <h3 className='bp3-heading'>
                 <a href={url} target='_blank' rel="noopener noreferrer">{playerName}</a>
             </h3>
             <Divider />
-            <h4 className='bp3-heading'>{stub}</h4>
-            <p>{result}</p>
-            {analysis}
+            <div className='player-card-body'>
+                <PlayerInfoTable player={player} />
+                <PlayerPredictionTable player={player} playerLookup={playerLookup} prediction={prediction} />
+                <div />
+                <div>{blurb}</div>
+            </div>
         </Card>
     )
 }
